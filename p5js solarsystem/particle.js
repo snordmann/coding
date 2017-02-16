19e4ad5d3a5cc7d61a1c02264c02bb3a1c9ed983
hablwgq <hablwgq@web.de>
2017-02-16 09:23:14 +0100

    Solarsystem; better structure, added Checkboxes

--- p5js solarsystem/particle.js
+++ p5js solarsystem/particle.js
@@ -1,72 +1,77 @@
 function Particle(position, velocity, mass) {
-  if(position) {
-	this.pos = position;
-  }else {
-	this.pos = createVector(random(width), random(height));
-  }
-  if(velocity) {
-	this.vel = velocity;
-  } else {
-	this.vel = p5.Vector.random2D(); //createVector(0, 0);
-  }
-  this.lastPos = [];
-  this.acc = createVector(0, 0);
-  if(mass) {
-	  this.mass = mass;
-  } else {
-	this.mass = random(1,5);
-  }
-  this.hu = random(360);
-  this.radius = 3*log(this.mass);
-
-  this.crash = false;
-  this.update = function() {
-	this.radius = 3*log(this.mass);
-    this.vel.add(this.acc);
-    this.pos.add(this.vel);
-    this.acc.mult(0);
-  }
-
-  this.attract = function(other) {
-	  if (p5.Vector.dist(other.pos, this.pos) <= this.radius + other.radius) {
-		  var velx = (this.vel.x * this.mass + other.vel.x * other.mass) / (this.mass + other.mass);
-		  var vely = (this.vel.y * this.mass + other.vel.y * other.mass) / (this.mass + other.mass);
-		  var velmag = sqrt(pow(velx,2) + pow(vely,2));
-		  var heading = atan(vely/velx);
-		  
-		  var tmpvel = p5.Vector.fromAngle(heading);
-		  tmpvel.setMag(velmag);
-		  if (this.mass <= other.mass) {
-			  other.vel = tmpvel;
-			  other.mass += this.mass;
-			  this.crash = true;
-		  } else {
-			  this.vel = tmpvel;
-			  this.mass += other.mass;
-			  other.crash = true;
-		  }
-	  }
-    var force = p5.Vector.sub(other.pos, this.pos);
-	var dis = p5.Vector.dist(this.pos, other.pos);
-	var strength = grav * other.mass / (dis*dis);
-	force.setMag(strength);
-    this.applyForce(force);
-  }
-
-  this.applyForce = function(force) {
-    this.acc.add(force);
-  }
-
-  this.show = function() {
-	  for (var i = 0; i < this.lastPos.length - 1; i++) {
-		stroke(this.hu,255,255,i/100);
-		  line(this.lastPos[i].x,this.lastPos[i].y,this.lastPos[i+1].x,this.lastPos[i+1].y)
-	  }
-	fill(this.hu,255,255);
-    ellipse(this.pos.x, this.pos.y, this.radius*2, this.radius*2);
-	this.lastPos.push(createVector(this.pos.x,this.pos.y));
-	if(this.lastPos.length > 100) {
-		this.lastPos.splice(0,1);
+	if(position) {
+		this.pos = position;
+	} else {
+		this.pos = createVector(random(width), random(height));
 	}
-  }
+	if(velocity) {
+		this.vel = velocity;
+	} else {
+		this.vel = p5.Vector.random2D(); //createVector(0, 0);
+	}
+	if(mass) {
+		this.mass = mass;
+	} else {
+		this.mass = random(1,5);
+	}
+	this.lastPos = [];
+	this.trail = showTrail.checked();
+	this.acc = createVector(0, 0);
+	this.hu = random(360);
+	this.radius = 1;
+	this.crash = false;
+	
+	this.update = function() {
+		this.radius = sqrt(this.mass);
+		this.vel.add(this.acc);
+		this.pos.add(this.vel);
+		this.acc.mult(0);
+	}
+
+	this.attract = function(other) {
+		if (p5.Vector.dist(other.pos, this.pos) <= this.radius + other.radius) {
+			var velx = (this.vel.x * this.mass + other.vel.x * other.mass) / (this.mass + other.mass);
+			var vely = (this.vel.y * this.mass + other.vel.y * other.mass) / (this.mass + other.mass);
+			var velmag = sqrt(pow(velx,2) + pow(vely,2));
+			var heading = atan(vely/velx);
+
+			var tmpvel = p5.Vector.fromAngle(heading);
+			tmpvel.setMag(velmag);
+			if (this.mass <= other.mass) {
+				other.vel = tmpvel;
+				other.mass += this.mass;
+				this.crash = true;
+			} else {
+				this.vel = tmpvel;
+				this.mass += other.mass;
+				other.crash = true;
+			}
+		}
+		var force = p5.Vector.sub(other.pos, this.pos);
+		var dis = p5.Vector.dist(this.pos, other.pos);
+		var strength = grav * other.mass / (dis*dis);
+		force.setMag(strength);
+		this.applyForce(force);
+	}
+
+	this.applyForce = function(force) {
+		this.acc.add(force);
+	}
+
+	this.show = function() {
+		stroke(this.hu, 255, 255);
+		if(this.trail) {
+			var len = this.lastPos.length;
+			for (var i = 0; i < len - 1; i++) {
+				line(this.lastPos[i].x,this.lastPos[i].y,this.lastPos[i+1].x,this.lastPos[i+1].y)
+			}
+		}
+		fill(this.hu,255,255);
+		ellipse(this.pos.x, this.pos.y, this.radius*2, this.radius*2);
+
+		this.lastPos.push(createVector(this.pos.x,this.pos.y));
+		if(len > 50) {
+			this.lastPos.splice(0,1);
+		}
+	}
 }
--- p5js solarsystem/sketch.js
+++ p5js solarsystem/sketch.js
@@ -1,9 +1,28 @@
 particles = [];
 var grav = 5;
+var showTrail;
+var deleteoffscreen;
+
+function toggleTrail() {		
+	if (this.checked()) {
+		for (var i in particles)
+			particles[i].trail = true;
+	} else {
+		for (var i in particles)
+			particles[i].trail = false;
+	}
+}
 
 function setup() {
 	createCanvas(400, 400);
 	colorMode(HSB);
+	
+	showTrail = createCheckbox('Show Trails', true);
+	showTrail.position(width + 50, 50);
+	showTrail.changed(toggleTrail);	
+	
+	deleteoffscreen = createCheckbox('Delete Offscreen', true);
+	deleteoffscreen.position(width + 50, 75);
 
 	for (var i = 0; i < 15; i++) {
 		particles.push(new Particle());
@@ -13,22 +32,32 @@ function setup() {
 function draw() {
 	background(0);
 	for (var i = particles.length - 1; i >= 0 ; i--) {
+		var death = false;
 		for (var j in particles) {
 			if (particles[j] != particles[i]) {
 				particles[i].attract(particles[j]);
 			}
 		}
-		if (particles[i].crash ||
-			particles[i].pos.x > width ||
-			particles[i].pos.x < 0 ||
-			particles[i].pos.y > height ||
-			particles[i].pos.y < 0) {
+		if(deleteoffscreen.checked()) {
+			if (particles[i].crash ||
+				particles[i].pos.x > width ||
+				particles[i].pos.x < 0 ||
+				particles[i].pos.y > height ||
+				particles[i].pos.y < 0) {
+					death = true;
+				}
+		} else {
+			if (particles[i].crash) {
+					death = true;
+				}
+		}
+		if (death) {
 			particles.splice(i,1);
 			particles.push(new Particle());
-			continue;
-			}
-		particles[i].update();
-		particles[i].show();
+		} else {
+			particles[i].update();
+			particles[i].show();
+		}
 	}
 
 }
